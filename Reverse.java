5d88c202eeec459d5f64bcabc20de5f12c89d3e6
Saurabh z <101171252+saur1999@users.noreply.github.com>
2022-03-29 14:28:07 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		
+		String str = "cdac";
+		String str1=""; 
+		
+	
+		
+		
+		
+		for(int i =str.length()-1;i>=0;--i) {
+			str1+=str.charAt(i);
+		}
+		System.out.println(str1);
+		
+	
+
+}}
